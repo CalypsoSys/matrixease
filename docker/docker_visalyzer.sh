717fb8049dbe7b9ee192bc68775535be5c34814a
Joe <joseph@calypsosys.com>
2022-02-05 14:53:30 -0500

    bash helper

--- docker/docker_visalyzer.sh
+++ docker/docker_visalyzer.sh
@@ -5,23 +5,35 @@ if [ "$op" == "start" ]; then
     OP=start
 fi
 
+
 if [ "$op" == "stop" ]; then
     OP=stop
 fi
 
 cd ~/docker/visalyzer_blog_wp
+echo "visalyzer_blog_wp $OP"
 sudo docker-compose $OP
+echo ""
 
 cd ~/docker/visalyzer_docs_static
+echo "vi visalyzer_docs_static $OP"
 sudo docker-compose $OP
+echo ""
 
 cd ~/docker/visalyzer_dotnet
+echo "visalyzer_dotnet $OP"
 sudo docker-compose $OP
+echo ""
 
 cd ~/docker/visalyzer_static
+echo "visalyzer_static $OP"
 sudo docker-compose $OP
+echo ""
 
 cd ~/docker/visalyzer_support_wp
+echo "visalyzer_support_wp $OP"
 sudo docker-compose $OP
+echo ""
 
 cd ~/docker
+
