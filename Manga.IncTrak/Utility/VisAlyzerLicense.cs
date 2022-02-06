8e4e0d882ff83c49da3bd84623dd337bbee09a8f
Joe <joseph@calypsosys.com>
2022-02-06 07:30:14 -0500

    5 projects 5 million

--- Manga.IncTrak/Utility/VisAlyzerLicense.cs
+++ Manga.IncTrak/Utility/VisAlyzerLicense.cs
@@ -9,10 +9,10 @@ namespace Manga.IncTrak.Utility
 {
     public static class VisAlyzerLicense
     {
-        private const int MaxProjects = 4;
-        private const string MaxProjectsMessage = "The free version limits the ability to save more than 4 distinct analysis projects for personal use. Contact us to get a license to remove this restriction or to enquire about a Enterprise license.";
-        private const int MaxRowsCols = 2000000;
-        private const string MaxCellsMessage = "The free version limits a input matrix to a maximum of 2 million cells for personal use. Contact us to get a license to remove this restriction or to enquire about a Enterprise license.";
+        private const int MaxProjects = 5;
+        private const string MaxProjectsMessage = "The free version limits the ability to save more than 5 distinct analysis projects for personal use. Contact us to get a license to remove this restriction or to enquire about a Enterprise license.";
+        private const int MaxRowsCols = 5000000;
+        private const string MaxCellsMessage = "The free version limits a input matrix to a maximum of 5 million cells for personal use. Contact us to get a license to remove this restriction or to enquire about a Enterprise license.";
 
         public static void CheckProjectCount(int mangas)
         {
