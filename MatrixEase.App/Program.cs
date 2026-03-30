3a23e3bf0311b3bc7bbdaf875f74414cbb8fe514
joe <joseph@calypsosys.com>
2026-03-30 05:52:35 -0400

    migrate app to electronnet core

--- MatrixEase.App/Program.cs
+++ MatrixEase.App/Program.cs
@@ -1,17 +1,20 @@
 using ElectronNET.API;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using ElectronNET.API.Entities;
+using MatrixEase.Manga.Manga.Serialization;
 
 namespace Desktop.MatrixEase.Manga
 {
     public class Program
     {
+        private static readonly Dictionary<string, object> _data = new Dictionary<string, object>();
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -21,8 +24,71 @@ namespace Desktop.MatrixEase.Manga
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseElectron(args);
+                    webBuilder.UseElectron(args, ElectronAppReady);
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static async Task ElectronAppReady()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("WSL_DISTRO_NAME")))
+            {
+                Electron.App.CommandLine.AppendArgument("--disable-gpu");
+                Electron.App.CommandLine.AppendArgument("--disable-software-rasterizer");
+            }
+
+            string userPath = await Electron.App.GetPathAsync(PathName.AppData);
+            MangaRoot.SetRootFolder(userPath);
+
+            var browserWindow = await Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions
+            {
+                Width = 1152,
+                Height = 940,
+                Show = false,
+                Icon = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets\\icon.ico"),
+                WebPreferences = new WebPreferences
+                {
+                    EnableRemoteModule = true,
+                    NodeIntegration = true,
+                }
+            });
+
+            await browserWindow.WebContents.Session.ClearCacheAsync();
+
+            browserWindow.OnReadyToShow += () => browserWindow.Show();
+            browserWindow.SetTitle("MatrixEase");
+#if !DEBUG_JAVASCRIPT
+            browserWindow.RemoveMenu();
+#endif
+
+            Electron.IpcMain.On("open_window", args =>
+            {
+                var jsonArgs = args as Newtonsoft.Json.Linq.JArray;
+                Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions
+                {
+                    AutoHideMenuBar = true,
+                    Icon = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets\\icon.ico"),
+                    WebPreferences = new WebPreferences
+                    {
+                        EnableRemoteModule = true,
+                        NodeIntegration = true,
+                    }
+                }, jsonArgs[0].ToString());
+
+                _data.Add(jsonArgs[1].ToString(), jsonArgs[2]);
+            });
+
+            Electron.IpcMain.On("opened_window", args =>
+            {
+                string id = args as string;
+                if (_data.ContainsKey(id))
+                {
+                    foreach (var win in Electron.WindowManager.BrowserWindows)
+                    {
+                        Electron.IpcMain.Send(win, "send_data", _data[id]);
+                    }
+                    _data.Remove(id);
+                }
+            });
+        }
     }
 }
--- MatrixEase.App/Startup.cs
+++ MatrixEase.App/Startup.cs
@@ -1,21 +1,12 @@
 //#define DEBUG_JAVASCRIPT
-using ElectronNET.API;
-using ElectronNET.API.Entities;
-using MatrixEase.Manga.Manga.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Desktop.MatrixEase.Manga
 {
@@ -64,76 +55,6 @@ namespace Desktop.MatrixEase.Manga
                     AppDomain.CurrentDomain.BaseDirectory, "wwwroot")),
             });
 #endif
-
-            ElectronBootstrap();
-        }
-
-        private Dictionary<string, object> _data = new Dictionary<string, object>();
-
-        public async void ElectronBootstrap()
-        {
-            var userPath = Electron.App.GetPathAsync(PathName.AppData);
-            userPath.Wait();
-            MangaRoot.SetRootFolder(userPath.Result);
-
-            var browserWindow = await Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions
-            {
-                Width = 1152,
-                Height = 940,
-                Show = false,
-                Icon = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets\\icon.ico"),
-                WebPreferences = new WebPreferences 
-                { 
-                    EnableRemoteModule = true,
-                    NodeIntegration = true,
-                    //NativeWindowOpen = true 
-                }
-            });
-
-            await browserWindow.WebContents.Session.ClearCacheAsync();
-
-            browserWindow.OnReadyToShow += () => browserWindow.Show();
-            browserWindow.SetTitle("MatrixEase");
-#if !DEBUG_JAVASCRIPT
-            browserWindow.RemoveMenu();
-#endif
-
-            //require('electron').ipcRenderer.send("open_window")
-            ElectronNET.API.Electron.IpcMain.On("open_window", (args) =>
-            {
-                var jsonArgs = args as Newtonsoft.Json.Linq.JArray;
-                var browserWindow = Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions
-                {
-                    AutoHideMenuBar = true,
-                    Icon = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets\\icon.ico"),
-                    WebPreferences = new WebPreferences
-                    {
-                        EnableRemoteModule = true,
-                        NodeIntegration = true,
-                    }
-                }, jsonArgs[0].ToString());
-
-                _data.Add(jsonArgs[1].ToString(), jsonArgs[2] );
-            });
-
-            ElectronNET.API.Electron.IpcMain.On("opened_window", (args) =>
-            {
-                var id = args as string;
-                if (_data.ContainsKey(id))
-                {
-                    foreach (var win in Electron.WindowManager.BrowserWindows)
-                    {
-                        ElectronNET.API.Electron.IpcMain.Send(win, "send_data", _data[id]);
-                    }
-                    _data.Remove(id);
-                }
-            });
-
-            Electron.App.BrowserWindowCreated += App_BrowserWindowCreated;
-        }
-
-        private void App_BrowserWindowCreated()
-        {
         }
     }
 }
